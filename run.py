73f49b7b3c3be91c7ae9b24ec5608f4908605296
sethusaim <sethusaim@gmail.com>
2022-10-20 16:42:58 +0530

    updated code and added data file path

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import dask.dataframe as dd
 
-col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
-
-df = dd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',names=col_names)
+df = dd.read_csv('Iris.csv')
 
 print(df.head())
         
