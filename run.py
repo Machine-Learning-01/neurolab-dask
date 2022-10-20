509f6b9ca31798285279f14e761d8b85b02f0590
sethusaim <sethusaim@gmail.com>
2022-10-20 15:18:29 +0530

    updated code

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import dask.dataframe as dd
 
-def read_dask_data():
-    try:
-        col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
+col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
 
-        df = dd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',names=col_names)
+df = dd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',names=col_names)
 
-        return df.head()
+print(df.head())
         
-    except Exception as e:
-        raise e 
-    
